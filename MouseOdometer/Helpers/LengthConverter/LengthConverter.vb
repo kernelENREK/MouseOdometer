2bc7192b78ffb6cf5eeca753687dd5250c694d51
admin <admin@TITAN>
2016-08-19 17:55:09 +0200

    removed unnecessary try-catch

--- MouseOdometer/Helpers/LengthConverter/LengthConverter.vb
+++ MouseOdometer/Helpers/LengthConverter/LengthConverter.vb
@@ -8,24 +8,20 @@
         Public Converter As ILength
 
         Public Sub New(provider As Providers)
-            Try
-                Select Case provider
-                    Case Providers.Meters
-                        Converter = New ConverterBase(1, "meters")
-                    Case Providers.Kilometers
-                        Converter = New ConverterBase(0.001, "kilometers")
-                    Case Providers.Miles
-                        Converter = New ConverterBase(0.000621371, "miles")
-                    Case Providers.Feets
-                        Converter = New ConverterBase(3.28084, "feets")
-                    Case Providers.Yards
-                        Converter = New ConverterBase(1.09361, "yards")
-                    Case Providers.Inches
-                        Converter = New ConverterBase(39.3701, "inches")
-                End Select
-            Catch ex As Exception
-                Throw New Exception(ex.Message)
-            End Try
+            Select Case provider
+                Case Providers.Meters
+                    Converter = New ConverterBase(1, "meters")
+                Case Providers.Kilometers
+                    Converter = New ConverterBase(0.001, "kilometers")
+                Case Providers.Miles
+                    Converter = New ConverterBase(0.000621371, "miles")
+                Case Providers.Feets
+                    Converter = New ConverterBase(3.28084, "feets")
+                Case Providers.Yards
+                    Converter = New ConverterBase(1.09361, "yards")
+                Case Providers.Inches
+                    Converter = New ConverterBase(39.3701, "inches")
+            End Select
         End Sub
 
         ''' <summary>
